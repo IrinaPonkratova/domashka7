cbbd68acf0bb5bee1f0570f5ff70b0aedd0bdef2
IrinaPonkratova <ira.ponkratova97@mail.ru>
2023-02-04 20:46:13 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int InputData(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] CreateArray(string msg)
+{
+    
+    int size = InputData(msg); 
+    int [] mass = new int [size];
+    return mass;
+}
+void FillArray(int [] array, string msg)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        array[i] =  InputData(msg);
+    }
+
+}
+int CheckPositive (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if (array[i] > 0)
+        {
+            count  +=1;
+        }
+    }
+    return count;
+}
+
+
+int [] arr = CreateArray("сколько чисел вы хотите ввести?");
+FillArray(arr, "введите число");
+System.Console.WriteLine(CheckPositive(arr));
---
+++ 2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+const int b1 = 0;
+const int k1 = 1;
+const int b2 = 2;
+const int k2 = 3;
+
+void UserInput(double[] koeff, string msg)
+{
+    System.Console.WriteLine($"введите коэффициенты b1, k1, b2, k2");
+    for (int i = 0; i <= koeff.Length - 1; i++)
+    {
+        koeff[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+double CheckPointX(double[] koeff)
+{
+    double x = (koeff[b2] - koeff[b1]) / (koeff[k1] - koeff[k2]);
+    return x;
+}
+double CheckPointY(double [] koeff)
+{
+    double x = CheckPointX(koeff);
+    double y = koeff[k1] * x + koeff[b1];
+    return y;
+}
+
+double [] koeff = new double[4];
+
+UserInput(koeff, "введите коэффициенты");
+double resultX = CheckPointX(koeff);
+double resultY = CheckPointY(koeff);
+System.Console.WriteLine($"точка пересечения двух прямых: {resultX}, {resultY}");
